8dd8265bae8a76e31bf1d109a090f9bfc80b6b74
AmazonianPanda <howyoudoing1618@gmail.com>
2020-10-25 14:10:45 +0530

    cont frac II added

---
+++ Continued Fractions-II.py
@@ -0,0 +1,32 @@
+
+
+import math
+
+def getPi(noOfIterations):
+    piValue = math.pow(noOfIterations, 2) / (noOfIterations*2 + 1)
+    noOfIterations -= 1
+    while(noOfIterations > 0):
+        piValue = pow(noOfIterations, 2) / ((noOfIterations*2 + 1) + piValue)
+        noOfIterations -= 1
+    piValue += 1
+    return 4/piValue
+
+
+
+valueOfPi = getPi(1)
+print("{0:.50f} and difference is {1}".format(valueOfPi, valueOfPi-math.pi))
+
+valueOfPi = getPi(10)
+print("{0:.50f} and difference is {1}".format(valueOfPi, valueOfPi-math.pi))
+
+valueOfPi = getPi(20)
+print("{0:.50f} and difference is {1}".format(valueOfPi, valueOfPi-math.pi))
+
+valueOfPi = getPi(50)
+print("{0:.50f} and difference is {1}".format(valueOfPi, valueOfPi-math.pi))
+
+valueOfPi = getPi(100)
+print("{0:.50f} and difference is {1}".format(valueOfPi, valueOfPi-math.pi))
+
+valueOfPi = getPi(1000)
+print("{0:.50f} and difference is {1}".format(valueOfPi, valueOfPi-math.pi))
